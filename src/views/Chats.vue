6f8c5afceea72a17c8a0d2b7bbf61fe8886d46f1
Artem <myartoym@gmail.com>
2024-06-13 12:26:38 +0300

    fix chats

--- src/views/Chats.vue
+++ src/views/Chats.vue
@@ -97,21 +97,13 @@ main {
   flex-direction: column;
   background-color: inherit;
   width: 100%;
-  min-height: 100vh;
   height: 100%;
 }
 .chats {
   display: flex;
   flex-direction: column;
   overflow: auto;
+  height: calc(100vh - 140px);
 }
-iframe {
-  width: 100%;
-  height: 100%;
-  background-color: inherit;
 
-  & body {
-    background-color: inherit;
-  }
-}
 </style>
--- src/views/ChatsRoute.vue
+++ src/views/ChatsRoute.vue
@@ -20,20 +20,5 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-main {
-  display: flex;
-  flex-direction: column;
-  background-color: inherit;
-  width: 100%;
-  min-height: 100vh;
-}
-iframe {
-  width: 100%;
-  height: 100%;
-  background-color: inherit;
 
-  & body {
-    background-color: inherit;
-  }
-}
 </style>
