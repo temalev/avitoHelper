38df36d59c140da6c090503ac463a51aed3fb132
Artem <myartoym@gmail.com>
2024-06-14 11:49:58 +0300

    Изменение пользователя

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,22 +1,84 @@
 <template>
   <main>
-   <span style="color: #fff">ID: {{ store.user?.id }}</span>
+    <TheTabs :data="items" />
+    <div class="container">
+      <div class="d-flex-column">
+        <span class="mb-4" style="color: #fff">ID: {{ store.user?.id }}</span>
+        <div class="d-flex-column gap-4">
+          <div v-if="this.form" class="d-flex-column gap-1">
+            <label for="name" style="color: #fff">Имя</label>
+            <InputText
+              id="name"
+              v-model="form.name"
+              class="flex-auto"
+              style="width: 300px"
+              autocomplete="off"
+            />
+          </div>
+          <div v-if="this.form" class="d-flex-column gap-1">
+            <label for="email" style="color: #fff">Email</label>
+            <InputText
+              id="email"
+              v-model="form.email"
+              class="flex-auto"
+              style="width: 300px"
+              autocomplete="off"
+            />
+          </div>
+          <Button label="Изменить пароль" text />
+          <Button :loading="updatindUserProcess" type="button" label="Сохранить" @click="updateUser" style="width: 160px" />
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 <script>
 import { useUserStore } from '@/stores/user'
-
+import {updateUser} from '@/api/user'
+import {getMe} from '@/api/login'
 export default {
   data() {
     return {
       store: useUserStore(),
+      items: [
+        { id: 1, label: 'Пользователь', icon: 'pi pi-home' },
+        { id: 2, label: 'Реферальная система', icon: 'pi pi-chart-line' },
+      ],
+      form: null,
+      updatindUserProcess: false,
     }
   },
 
   mounted() {
+    this.getMe()
   },
+
   methods: {
-   
+    async updateUser() {
+      this.updatindUserProcess = true;
+      const data = {
+        name: this.form.name,
+        email: this.form.email
+      }
+      try {
+        const res = await updateUser(data)
+        this.getMe()
+      }
+      catch(e) {
+        console.error(e);
+      }
+      this.updatindUserProcess = false;
+    },
+    async getMe() {
+      try {
+        const res = await getMe()
+        this.form = JSON.parse(JSON.stringify(res))
+        this.store.user = JSON.parse(JSON.stringify(res))
+      }
+      catch(e) {
+        console.error(e);
+      }
+    }
   }
 }
 </script>
@@ -26,15 +88,17 @@ main {
   flex-direction: column;
   background-color: inherit;
   width: 100%;
-  min-height: 100vh;
 }
-iframe {
+.container {
+  padding: 12px;
   width: 100%;
-  height: 100%;
-  background-color: inherit;
-
-  & body {
-    background-color: inherit;
-  }
+  height: 500px;
+  /* From https://css.glass */
+  background: var(--background-color);
+  border-radius: 0 8px 8px 8px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(4.9px);
+  -webkit-backdrop-filter: blur(4.9px);
+  border: 1px solid var(--border-color);
 }
 </style>
